3501b30489c0bd8485f2f15bcc0a9701dd86dabb
Kalan Jarvis-Loewen <karj5903@gmail.com>
2025-04-13 20:36:30 -0700

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Footer from "@/components/Footer";
 import QualityWrapper from "@/app/QualityWrapper";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "JMAI.PHOTOS",
@@ -25,7 +26,10 @@ export default function RootLayout({
         <meta property="og:image" content="https://www.jmai.photos/whiteicon.png" />
       </Head>
       <body className="overflow-x-hidden">
-        <QualityWrapper>{children}</QualityWrapper>
+        <QualityWrapper>
+          <Analytics />
+          {children}
+          </QualityWrapper>
         <Footer />
       </body>
     </html>
