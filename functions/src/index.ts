d938b39b1fd6dcb020ce7457ed690b5612a183b5
Kalan Jarvis-Loewen <112147975+kalandjl@users.noreply.github.com>
2026-03-21 16:11:19 -0400

    twilio working

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,7 +7,7 @@ admin.initializeApp();
 
 exports.twillioFirestoreReroute = onDocumentWritten({
     document: "messages/{docId}",
-    secrets: ["TWILIO_ACCOUNT_SID", "TWILIO_AUTH_TOKEN"],
+    secrets: ["TWILIO_ACCOUNT_SID", "TWILIO_AUTH_TOKEN", "CLIENT_PHONE_NUMBER"],
   }, async (event) => {
   const change: any = event.data;
 
@@ -46,19 +46,60 @@ exports.twillioFirestoreReroute = onDocumentWritten({
 
         const accountSid = process.env.TWILIO_ACCOUNT_SID
         const authToken = process.env.TWILIO_AUTH_TOKEN
+        const clientPhoneNumber = process.env.CLIENT_PHONE_NUMBER
+
         const client = require('twilio')(accountSid, authToken);
-        client.messages
+        const message1 = await client.messages
             .create({
                 body: `New message from "${messageData.name}", "${messageData.email}"Message: "${messageData.message}"`,
                 from: '+13658326339',
                 to: '+12366680975'
             })
-            .then((message: any) => console.log(message.sid));
+
+        const message2 = await client.messages
+            .create({
+                body: `New message from "${messageData.name}", "${messageData.email}"Message: "${messageData.message}"`,
+                from: '+13658326339',
+                to: clientPhoneNumber
+            })
+
+        console.log("Message 1 status:", message1.status);
+        console.log("Message 2 status:", message2.status);
+
+        const smsRef = admin.firestore().collection("logs").doc();
+
+        await smsRef.set({
+            ...messageData,
+            createdAt: admin.firestore.FieldValue.serverTimestamp(),
+            messages: [
+            {
+                to: "+12366680975",
+                sid: message1.sid,
+                status: message1.status,
+            },
+            {
+                to: clientPhoneNumber,
+                sid: message2.sid,
+                status: message2.status,
+            },
+            ],
+        });
 
 
-      } catch (error) {
-        logger.error("Error adding document to 'sms' collection:", error);
-      }
+
+
+      } catch (error: any) {
+            console.error("Twilio error:", error);
+
+            const smsRef = admin.firestore().collection("logs").doc();
+
+            await smsRef.set({
+                ...messageData,
+                createdAt: admin.firestore.FieldValue.serverTimestamp(),
+                error: error.message,
+                status: "failed",
+        });      
+        }
     }
   } else {
     logger.info("Document was deleted or does not exist after the change.");
